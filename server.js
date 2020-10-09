26fd20e8d4ec09669b9a914a53e8bdd9f3086682
Kirk Tolliver <kirktolliver28@gmail.com>
2020-10-08 23:30:02 -0500

    Made every mistake on my lonely road back home

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ server.get("/users/:id", (req,res) => {
 server.post("/api/users",(req,res) =>{
     const newUser =req.body
     if ( !newUser.name  || !newUser.bio ){
+        res.status(400)
        res.json( { errorMessage: "Please provide name and bio for the user." })
     }else {
         try{
@@ -37,22 +38,34 @@ server.post("/api/users",(req,res) =>{
             res.status(201)
             res.json(newPost)
         }catch(error){
-            console.log(error)
+            res.json({ errorMessage: "There was an error while saving the user to the database" })
             res.status(500)
         }
     }
 })
 
 // step 2
-server.get("/api/users/:id", (req, res) => {
-    const id = req.params.id
-    const user = db.getUserById(id)
-    res.json(user)
+server.get("/api/users/", (req, res) => {
+    try {
+        const users = db.getUsers()
+        res.json(users)
+    }catch{
+        res.status(500)
+        res.json( { errorMessage: "The users information could not be retrieved." })
+    }
 })
 
 // step 3
 server.get("/api/users/:id", (req, res) => {
-    res.json({})
+    const id = req.params.id
+    const user = db.getUserById(id)
+    if (user){
+        res.json(user)
+    }else{
+        res.status(404)
+        res.json({ message: "The user with the specified ID does not exist." })
+    }
+
 })
 
 // step 4 
@@ -75,6 +88,27 @@ server.delete("/api/users/:id", (req, res) => {
 // step 5 
 server.put("/api/users/:id", (req,res) => {
     //place code here
+    const id =req.params.id
+    const user = db.getUserById(id)
+    const data = req.body
+    if (user) {
+         if ( !data.name  || !data.bio ){
+             res.status(400)
+            res.json( { errorMessage: "Please provide name and bio for the user." })
+         }else {
+             try{
+                 const updated = db.updateUser(id, {
+                     name: req.body.name,
+                     bio: req.body.bio
+                 })
+                 res.status(201)
+                 res.json(updated)
+             }catch(error){
+                 res.json({ errorMessage: "There was an error while saving the user to the database" })
+                 res.status(500)
+             }
+         }
+    }
 })
 
 server.listen(8080, () => {
